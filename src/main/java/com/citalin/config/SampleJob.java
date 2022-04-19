3fb81895415216092f3d2414e677d100b55727a9
arturich <arturich@gmail.com>
2022-04-19 08:36:27 -0500

    Changing DB from H2 to MySQL and setting it to initialize, also added the second tasklet to be a service autowiring it with qualifier notation

--- src/main/java/com/citalin/config/SampleJob.java
+++ src/main/java/com/citalin/config/SampleJob.java
@@ -2,13 +2,11 @@ package com.citalin.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,7 +20,14 @@ public class SampleJob {
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Autowired
+	@Qualifier("firstTaskleet")
+	Tasklet firstTasklet;
+	
+	@Autowired
+	@Qualifier("secondTasklet")
 	Tasklet secondTasklet;
+	
+	
 	
 	@Bean
 	public Job firstJob()
@@ -37,21 +42,21 @@ public class SampleJob {
 	private Step firstStep()
 	{
 		return stepBuilderFactory.get("First Step")
-		.tasklet(firstTask())
+		.tasklet(firstTasklet)
 		.build();
 	}
 	
-	private Tasklet firstTask()
-	{
-		return new Tasklet() {
-			
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("This is first tasklet step");
-				return RepeatStatus.FINISHED;
-			}
-		};
-	}
+//	private Tasklet firstTask()
+//	{
+//		return new Tasklet() {
+//			
+//			@Override
+//			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//				System.out.println("This is first tasklet step");
+//				return RepeatStatus.FINISHED;
+//			}
+//		};
+//	}
 	
 	private Step secondStep()
 	{
